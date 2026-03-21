b995fc48daa5eebdc507151138e0799f8f2e852a
Natthanan04 <natthanansirisurayut@gmail.com>
2026-03-21 23:45:56 +0700

    chore(core): update main routes and flutter plugin configurations

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'modules/login/login_controller.dart';
 import 'modules/login/login_screen.dart';
+import 'modules/home/home_screen.dart';
+import 'modules/package/popular_package_screen.dart';
+import 'modules/package/new_package_screen.dart';
+import 'modules/package/detail_package_screen.dart';
+import 'modules/package/search_package_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +31,24 @@ class CulturaApp extends StatelessWidget {
       initialBinding: BindingsBuilder(() {
         Get.lazyPut<LoginController>(() => LoginController());
       }),
-      home: const LoginScreen(),
+      initialRoute: '/login',
+      getPages: [
+        GetPage(name: '/login', page: () => const LoginScreen()),
+        GetPage(name: '/home', page: () => const HomeScreen()),
+        GetPage(
+          name: '/popularPackages',
+          page: () => const PopularPackageScreen(),
+        ),
+        GetPage(name: '/newPackages', page: () => const NewPackageScreen()),
+        GetPage(
+          name: '/packageDetail',
+          page: () => const PackageDetailScreen(),
+        ),
+        GetPage(
+          name: '/searchPackages',
+          page: () => const SearchPackageScreen(),
+        ),
+      ],
     );
   }
 }
